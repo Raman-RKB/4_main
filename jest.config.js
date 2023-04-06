50b7137e9b70525ad5032d085c767360d8ddd5bd
Raman-RKB <121190700+Raman-RKB@users.noreply.github.com>
2023-04-06 23:34:26 +0300

    init commit

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '\\.(css|less)$': 'identity-obj-proxy',
+        '\\.(gif|ttf|eot|svg|png|jpg)$': '<rootDir>/__mocks__/svgMock.js',
+    },
 }
